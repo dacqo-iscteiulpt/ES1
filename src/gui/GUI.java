b3edc2e9849e64ced1123f0dcff18a56f7a36826
David Queimado <dacqo@iscte-iul.pt>
2017-11-23 11:16:17 +0000

    GUI Middle

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -4,9 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
@@ -15,23 +17,25 @@ public class GUI {
 	private JFrame frame = new JFrame("GUI");
 
 	private JPanel top = new JPanel();
-	
 	private JLabel rules = new JLabel("rules.cf path: ");
 	private JLabel ham = new JLabel("ham.log path: ");
 	private JLabel spam = new JLabel("spam.log path: ");
-	
 	private JTextField rulesPath = new JTextField("");
 	private JTextField hamPath = new JTextField("");
 	private JTextField spamPath = new JTextField("");
 	
 	private JPanel middle = new JPanel();
 	
-	private JPanel middleBottom = new JPanel();
-	
-//	private JCheckBox
-	
 	private JPanel bottom = new JPanel();
-
+	private JLabel automatic = new JLabel("                             Automatic");
+	private JPanel bottomPanel = new JPanel();
+	private JPanel bottomLeft = new JPanel();
+	private JLabel rulesWeight = new JLabel("Rules            Weights");
+	private JTable bottomTable = new JTable(5,2);
+	private JPanel bottomRight = new JPanel();
+	private JButton makeConfig = new JButton("Make Config");
+	private JButton saveConfig = new JButton("Save Config");
+	
 
 	public void go() {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -39,7 +43,6 @@ public class GUI {
 			public void run() {
 				buildFrame();
 				buildTop();
-				buildMiddle();
 				buildBottom();
 				finishFrame();
 			}
@@ -59,15 +62,25 @@ public class GUI {
 	}
 
 
-	private void buildMiddle() {
-		middle.setLayout(new BorderLayout());
-		middleBottom.setLayout(new FlowLayout());
-		middle.add(middleBottom, BorderLayout.SOUTH);
-	}
-
-
 	private void buildBottom() {
-		// TODO Auto-generated method stub
+		bottom.setLayout(new BorderLayout());
+		bottom.add(automatic, BorderLayout.NORTH);		
+		bottom.add(bottomPanel);
+		
+		bottomPanel.setLayout(new FlowLayout());
+		bottomPanel.add(bottomLeft);
+		
+		bottomLeft.setLayout(new BorderLayout());
+		bottomLeft.add(rulesWeight, BorderLayout.NORTH);
+		bottomLeft.add(bottomTable);
+		
+		bottomRight.setLayout(new GridLayout(2,1));
+		bottomRight.add(makeConfig);
+		bottomRight.add(saveConfig);
+		
+		bottomPanel.add(bottomRight);
+		frame.add(bottom);
+		
 	}
 
 
