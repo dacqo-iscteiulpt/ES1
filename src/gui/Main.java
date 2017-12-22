c85e90bc2779cba107b447fd338b58860f6235c9
micaelealprata <micaelealprata@gmail.com>
2017-12-22 23:43:35 +0000

    final version

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		GUI gui = new GUI();
 		gui.go();
 		
+		//generate HV.Boxplot.eps
 		String[] params = new String [2];
 
 		params[0] = "C:/Program Files/R/R-3.4.3/bin/x64/Rscript.exe";
@@ -18,23 +19,26 @@ public class Main {
 		String[] envp = new String [1];
 
 		envp[0] = "Path=C:/Program Files/R/R-3.4.3/bin/x64";
-		
+
 		try {
-			Process p = Runtime.getRuntime().exec(params, envp, new File("C:/Users/Micael/git/ES1-2017-IC1-65/config/AntiSpamStudy/R"));
+			Process p1 = Runtime.getRuntime().exec(params, envp, new File("C:/Users/Micael/git/ES1-2017-IC1-65/config/AntiSpamStudy/R"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		//generate AntiSpamStudy.pdf 
+		params[0] = "C:/Users/Micael/AppData/Local/Programs/MiKTeX 2.9/miktex/bin/x64/pdflatex.exe";
+
+		params[1] = "C:/Users/Micael/git/ES1-2017-IC1-65/config/AntiSpamStudy/latex/AntiSpamStudy.tex";
+
+		envp[0] = "Path=C:/Users/Micael/AppData/Local/Programs/MiKTeX 2.9/miktex/bin/x64";
 		
-		ProcessBuilder pb = new ProcessBuilder("pdflatex", "-shell-escape", "AntiSpamStudy.tex");
-        pb.directory(new File("C:/Users/Micael/git/ES1-2017-IC1-65/config/AntiSpamStudy/latex"));
-        try {
-            Process p = pb.start();
-            p.waitFor();
-        } catch (IOException | InterruptedException ex) {
-            ex.printStackTrace();
-        }
-
+		try {
+			Process p2 = Runtime.getRuntime().exec(params, envp, new File("C:/Users/Micael/git/ES1-2017-IC1-65/config/AntiSpamStudy/latex"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
